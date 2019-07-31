91c4e681c438250e64b58d1c8dc20e888d3f978c
seiyasolo <seiyasolo@gmail.com>
2019-07-31 17:40:35 +0800

    添加webpack配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,18 +1,38 @@
 const path = require('path');
-
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'build.js',
-        path: path.resolve(__dirname, 'dist');
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    resolve: {
+        // 设置扩展，这样导入文件时可以省去写扩展名
+        extensions: [".js", ".json", ".jsx"]
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                use: 'file-loader'
             }
-        }]
-    }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        })
+    ]
 };
